3abbf2083ea390dabcfa170f0e9220d9e1fb0dad
Marta Wlusek <wlusek.marta@gmail.com>
2020-09-09 13:29:15 +0200

    Corrections to javascript exercise

--- 8September/8_sept.js
+++ 8September/8_sept.js
@@ -1,24 +1,25 @@
 //1
 function logType(x) {
-    if (typeof x == undefined) {
+    if (typeof x == "undefined") {
         console.log("undefined!");
-    } else if (typeof x == null) {
+    } else if (x == null) {
         console.log("null!");
-    } else if (typeof x == Number) {
+    } else if (typeof x == "number") {
         console.log("number!");
-    } else if (typeof x == NaN) {
+    } else if (x == isNaN) {
         console.log("not a number!");
     } else if (typeof x == "string") {
         console.log("string");
     } else if (Array.isArray(x)) {
         console.log("array!");
-    } else if (typeof x == {}) {
+    } else if (typeof x == "object") {
         console.log("object!");
     } else if (typeof x == "function") {
         console.log("function!");
-    } else if (typeof x == console.log("I have no idea!"));
+    } else {console.log("I have no idea!");}
 }
 logType();
+
 
 //2
 var a = {
