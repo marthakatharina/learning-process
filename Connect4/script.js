6b0a1e9e7515c243fc79d01e771ff19fc84ea286
Marta Wlusek <wlusek.marta@gmail.com>
2020-09-26 00:15:19 +0200

    Connect Four in progress - update

--- Connect4/script.js
+++ Connect4/script.js
@@ -1,22 +1,15 @@
 (function () {
-    // console.log($);
-
     var currentPlayer = "player1";
 
     $(".column").on("click", function (e) {
-        // console.log("click on column");
-
-        var col = $(e.currentTarget); // currentTarget is a method like target
-        // console.log("col: ", col);
+        var col = $(e.currentTarget);
         var slotsInCol = col.children();
-        // console.log(slotsinCol);
+
         for (var i = slotsInCol.length - 1; i >= 0; i--) {
-            // console.log(slotsInCol.eg(i).hasClass("player1")); // hasClass is jq method
             if (
                 !slotsInCol.eq(i).hasClass("player1") &&
                 !slotsInCol.eq(i).hasClass("player2")
             ) {
-                // console.log("time to do somethig");
                 slotsInCol.eq(i).addClass(currentPlayer);
                 break;
             }
@@ -28,17 +21,68 @@
         var slotsInRow = $(".row" + i);
         if (checkForVictory(slotsInCol)) {
             console.log("column victory");
+            // $(".victory").css({
+            //     visibility: "visible",
+            //     backgroundColor: "yellow",
+            // });
+
+            if (slotsInCol.hasClass("player1")) {
+                $(".victory").css({
+                    visibility: "visible",
+                    backgroundColor: "red",
+                });
+            } else {
+                $(".victory").css({
+                    visibility: "visible",
+                    backgroundColor: "yellow",
+                });
+            }
+            $("#overlay").css({
+                visibility: "visible",
+            });
+
+            setTimeout(function () {
+                $("button").css({ visibility: "visible" });
+            }, 2000);
         } else if (checkForVictory(slotsInRow)) {
             console.log("row victory");
+            $(".victory").css({
+                visibility: "visible",
+                backgroundColor: "blue",
+            });
+            $("#overlay").css({
+                visibility: "visible",
+            });
+
+            setTimeout(function () {
+                $("button").css({ visibility: "visible" });
+            }, 2000);
+        } else if (checkForDiagonals()) {
+            console.log("diag victory");
         }
+        // else {
+        //     console.log("no-one won!");
+        //     setTimeout(function () {
+        //         $("button").css({ visibility: "visible" });
+        //     }, 2000);
+
+        // }
         switchPlayer();
     });
 
+    function checkForDiagonals() {
+        var diags = [
+            [0, 7, 14, 21],
+            [6, 13, 20, 27],
+            [12, 19, 26, 33],
+        ];
+    }
     function checkForVictory(slots) {
         // here looping for slots
         var count = 0;
         for (var i = 0; i < slots.length; i++) {
-            if (slots.eq(i).hasClass(currentPlayer)) {
+            var slot = $(slots[i]); // refactoring
+            if (slot.hasClass(currentPlayer)) {
                 count++;
                 if (count === 4) {
                     return true;
@@ -49,16 +93,21 @@
         }
     }
 
+    $("button").click(function () {
+        location.reload();
+        // console.log("refresh");
+    });
+
     function switchPlayer() {
-        // if (currentPlayer === "player1") {
-        //     currentPlayer = "player2";
-        // } else {
-        //     currentPlayer = "player1";
-        // }
+        if (currentPlayer === "player1") {
+            currentPlayer = "player2";
+        } else {
+            currentPlayer = "player1";
+        }
 
         // other way (tenary operator):
 
-        currentPlayer = currentPlayer === "player1" ? "player2" : "player1";
+        // currentPlayer = currentPlayer === "player1" ? "player2" : "player1";
 
         // or
         // currentPlayer === "player1" ?
