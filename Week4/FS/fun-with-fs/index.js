b3868160cfb1e834da31086314ab7808ce80ff78
Marta Wlusek <wlusek.marta@gmail.com>
2020-10-03 16:51:33 +0200

    exercise completed without JSON

--- Week4/FS/fun-with-fs/index.js
+++ Week4/FS/fun-with-fs/index.js
@@ -2,32 +2,59 @@ const fs = require("fs");
 
 const fullPath = __dirname;
 
-function logSizes(path) {
-    // const fullPath = __dirname;
-    fs.readdir(path, { withFileTypes: true }, (err, content) => {
-        // console.log('content:', content);
-        if (err) {
-            console.log("something went wrong in writeFileTypes:", err);
-        }
+// function logSizes(path) {
+//     fs.readdir(path, { withFileTypes: true }, (err, content) => {
+//         // console.log('content:', content);
+//         if (err) {
+//             console.log("something went wrong in writeFileTypes:", err);
+//         }
 
-        for (let i = 0; i < content.length; i++) {
-            console.log("content name is:", content[i].name);
-            if (content[i].name) {
-                fs.stat(`${path}/${content[i].name}`, (err, data) => {
-                    // if (err) {
-                    //     // console.log("error in stat:", err);
-                    // }
-                    console.log(
-                        "stats for",
-                        `${path}/${content[i].name}:`,
-                        data.size
-                    );
-                });
-            } else {
-                logSizes(`${path}/${content[i].name}`);
-            }
+//         for (let i = 0; i < content.length; i++) {
+//             console.log("content name is:", content[i].name);
+//             if (content[i].isFile()) {
+//                 fs.stat(`${path}/${content[i].name}`, (err, data) => {
+//                     if (err) {
+//                         console.log("error in stat:", err);
+//                     }
+//                     console.log(
+//                         "stats for",
+//                         `${path}/${content[i].name}:`,
+//                         data.size
+//                     );
+//                 });
+//             } else {
+//                 logSizes(`${path}/${content[i].name}`);
+//             }
+//         }
+//     });
+// }
+
+// logSizes(fullPath);
+
+function mapSizes(path) {
+    const myDir = fs.readdirSync(path, { withFileTypes: true });
+    console.log("readdirSync has this value:", myDir);
+    let obj = {};
+    for (var i = 0; i < myDir.length; i++) {
+        console.log("name of the property is:", myDir[i].name);
+        if (myDir[i].isFile()) {
+            obj[myDir[i].name] = myDir[i].size;
+            const myStat = fs.statSync(`${path}/${myDir[i].name}`);
+            console.log(
+                `myStat for ${path}/${myDir[i].name} value is:`,
+                myStat
+            );
+        } else if (myDir[i].isDirectory()) {
+            obj[myDir[i].name] = mapSizes(`${path}/${myDir[i].name}`);
+        } else {
+            return obj;
         }
-    });
+    }
+    // mapSizes(`${path}/files`);
+
+    // var newFile = JSON.stringify(obj, null, 4);
+
+    // fs.writeFileSync(`${path}/files/files.json`, newFile);
 }
 
-logSizes(fullPath);
+mapSizes(fullPath);
