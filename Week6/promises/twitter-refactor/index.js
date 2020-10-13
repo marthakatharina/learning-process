cdee8f0ff6618660561aa0adc3d629673cea268c
Marta Wlusek <wlusek.marta@gmail.com>
2020-10-13 21:43:16 +0200

    TwitterAPI refactored with Promises

--- Week6/promises/twitter-refactor/index.js
+++ Week6/promises/twitter-refactor/index.js
@@ -19,37 +19,4 @@ app.get("/links.json", (req, res) => {
         });
 });
 
-/////// PROMISE.ALL ///////
-///// something has to be changed in export js to make it work - screen_name and user_id to get all 3 promisses (tweets form 3 different accounts at the same time)
-
-// app.get("/links.json", (req, res) => {
-//     getToken()
-//         .then((bearerToken) => {
-//             Promise.all([
-//                 getTweets(bearerToken, "zeitonline"),
-//                 getTweets(bearerToken, "guardian"),
-//                 getTweets(bearerToken, "rzeczpospolita"),
-//             ])
-//                 .then((results) => {
-//                     const mergedResults = [
-//                         ...results[0],
-//                         ...results[1],
-//                         ...results[2],
-//                     ];
-//                     const sortedTweets = mergedResults.sort((a, b) => {
-//                         return new Date(b.created_at) - new Date(a.created_at);
-//                     });
-//                     // filter tweets and send them back
-//                     const filteredTweets = filterTweets(sortedTweets);
-//                     res.json(filteredTweets);
-//                 })
-//                 .catch((err) => {
-//                     console.log("err in promise all: ", err);
-//                 });
-//         })
-//         .catch((err) => {
-//             console.log("err in catch: ", err);
-//         });
-// });
-
 app.listen(8080, () => console.log("twicker up and running"));
